80f3f81bf4eba7fcc2088bb9180b566f7c0ae728
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-11-24 21:07:05 -0300

    Add files via upload

---
+++ aprensent.java
@@ -0,0 +1,16 @@
+public class newJavaFile1 {
+
+
+    public static void main(String entrada[]) {
+       int idade = 20;
+       double altura = 1.76;
+       char genero = 'm';
+       String nome = "Carlos Araújo";
+       String curso = "ADS";
+       
+       System.out.println("Meu nome é " + nome + "tenho" + idade + "anos, minha altura é "
+       + altura + "e sou do curso de: " + curso);
+       
+       System.exit(0);
+    }
+}
